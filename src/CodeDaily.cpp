095bf6604dc26f8fa4b218099afa2056788896f4
wenwu.xiu <wayne.xiu@apimetrology.com>
2020-11-02 04:08:36 -0500

    update code with c# style prperties

--- src/CodeDaily.cpp
+++ src/CodeDaily.cpp
@@ -5,20 +5,47 @@
 #include <string>
 
 
-class Person
-{
+//class Person
+//{
+//public:
+//	int get_age() const { return age_; }
+//	void set_age(int value) { age_ = value; }
+//	// unfortunately, this doesn't work with GCC, for Clang, special setting is needed
+//	__declspec(property(get = get_age, put = set_age)) int age;
+//private:
+//	int age_ = 0;
+//};
+
+#include <functional>
+
+template<typename T>
+class property {
 public:
-	int get_age() const { return age_; }
-	void set_age(int value) { age_ = value; }
-	__declspec(property(get = get_age, put = set_age)) int age;
+    property(
+        std::function<void(T)> setter,
+        std::function<T()> getter) :
+        setter_(setter), getter_(getter) { }
+    operator T() const { return getter_(); }
+    property<T>& operator= (const T& value) { setter_(value); return *this; }
+    T& value() { return value_; }
 private:
-	int age_ = 0;
+    std::function<void(T)> setter_;
+    std::function<T()> getter_;
+    T value_;
+};
+
+class Person {
+public:
+    property<int> age {
+        [this](int x) { this->age.value() = x; },        // Setter ... yes you can do this!
+            [this]() -> int {  return this->age.value(); } // Getter.
+    };
 };
 
 int main() {
 
 	Person p;
-	std::cout << p.age << std::endl;
+	//std::cout << p.age << std::endl;
 	p.age = 20;
 	std::cout << p.age << std::endl;
 
